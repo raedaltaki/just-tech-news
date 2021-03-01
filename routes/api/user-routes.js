39e0cd94354f082d83becfcd26754a179c22d369
raedaltaki <raed.simon@gmail.com>
2021-03-01 05:02:59 -0500

    add encryption to password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) =>
     // Access our User model and run .findAll() method)
     User.findAll(
         {
-            attributes: { exclude: ['password'] }
+            // attributes: { exclude: ['password'] }
         })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => 
@@ -22,7 +22,7 @@ router.get('/:id', (req, res) =>
 {
     User.findOne(
         {
-            attributes: { exclude: ['password'] },
+            // attributes: { exclude: ['password'] },
             where: 
             {
                 id: req.params.id
@@ -70,6 +70,7 @@ router.put('/:id', (req, res) =>
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, 
         {
+            individualHooks: true,
             where: 
             {
                 id: req.params.id
