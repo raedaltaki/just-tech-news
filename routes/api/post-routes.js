08228ce31a59bbd29a031778c96c2dd68b3728cd
raedaltaki <raed.simon@gmail.com>
2021-03-04 01:33:14 -0500

    add Vote and comment

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
+const sequelize = require('../../config/connection');
+const { post } = require('./user-routes');
 
 // get all users
 router.get('/', (req, res) => 
@@ -7,20 +9,38 @@ router.get('/', (req, res) =>
     console.log('======================');
     Post.findAll(
     {
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: 
+        [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         order: [['created_at', 'DESC']], 
         include: 
         [
+            // include the Comment model here:
             {
-              model: User,
-              attributes: ['username']
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: 
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
             }
         ]
     })
     .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+    .catch(err => 
+    {
+        console.log(err);
+        res.status(500).json(err);
     });
 });
 
@@ -29,15 +49,31 @@ router.get('/:id', (req, res) => {
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
       include: [
+        // include the Comment model here:
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: 
+            {
+                model: User,
+                attributes: ['username']
+            }
+        },
         {
           model: User,
           attributes: ['username']
         }
       ]
     })
-      .then(dbPostData => {
+    .then(dbPostData => {
         if (!dbPostData) {
           res.status(404).json({ message: 'No post found with this id' });
           return;
@@ -62,6 +98,20 @@ router.post('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
+});
+
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => 
+{
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, {Vote})
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => 
+    {
+        console.log(err);
+        res.status(400).json(err);
+    });
+    
 });
 
 router.put('/:id', (req, res) => {
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User,Post,Vote, Comment } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => 
@@ -7,7 +7,7 @@ router.get('/', (req, res) =>
     // Access our User model and run .findAll() method)
     User.findAll(
         {
-            // attributes: { exclude: ['password'] }
+            attributes: { exclude: ['password'] }
         })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => 
@@ -22,11 +22,34 @@ router.get('/:id', (req, res) =>
 {
     User.findOne(
         {
-            // attributes: { exclude: ['password'] },
+            attributes: { exclude: ['password'] },
             where: 
             {
                 id: req.params.id
-            }
+            },
+            include: 
+            [
+                {
+                    model: Post,
+                    attributes: ['id', 'title', 'post_url', 'created_at']
+                },
+                // include the Comment model here:
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'created_at'],
+                    include: 
+                    {
+                        model: Post,
+                        attributes: ['title']
+                    }
+                },
+                {
+                    model: Post,
+                    attributes: ['title'],
+                    through: Vote,
+                    as: 'voted_posts'
+                }
+            ]
         })
         .then(dbUserData => 
         {
