d760facdbfcb6e4717f94415d6db9019657ea4aa
raedaltaki <raed.simon@gmail.com>
2021-03-08 06:32:37 -0500

    add single post page

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -12,19 +12,24 @@ Comment.findAll()
 });
 
 router.post('/', (req, res) => {
-    Comment.create(
+    
+      // check the session
+    if (req.session) 
     {
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(err => 
-    {
-        console.log(err);
-        res.status(400).json(err);
-    });
-
+        Comment.create(
+        {
+            comment_text: req.body.comment_text,
+            // use the id from the session
+            user_id: req.session.user_id,
+            post_id: req.body.post_id
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => 
+        {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
 });
 
 router.delete('/:id', (req, res) => {
